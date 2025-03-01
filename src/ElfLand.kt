54d463d88b4883d33d2a02c4337d20199c970b61
JossanTorner <josefintorner@gmail.com>
2025-03-01 11:28:35 +0100

    working function

--- src/ElfLand.kt
+++ src/ElfLand.kt
@@ -1,45 +1,66 @@
 
-/*
-Tomtarna på Nordpolen har en strikt chefs-hierarki
-Högsta chefen för allt är "Tomten"
-Under "Tomten" jobbar "Glader" och "Butter"
-Under "Glader" jobbar "Tröger", "Trötter"och "Blyger"
-Under "Butter" jobbar "Rådjuret", "Nyckelpigan", "Haren" och "Räven"
-Under "Trötter" jobbar "Skumtomten"
-Under "Skumtomten" jobbar "Dammråttan"
-Under "Räven" jobbar "Gråsuggan" och "Myran"
-Under "Myran" jobbar "Bladlusen"
-
-Er uppgift är att illustrera tomtens arbetshierarki i en lämplig datastruktur.
-(Det finns flera tänkbara datastrukturer här)
-
-Skriv sedan en funktion där man anger ett namn på tomten eller någon av hens underhuggare som
-inparameter.
-Funktionen listar sedan namnen på alla underlydandesom en given person har
-Expempel: Du anger "Trötter" och får som svar ["Skumtomten", "Dammråttan"]"
-
-För att bli godkänd på uppgiften måste du använda rekursion.
-
- */
-
-//TODO: skapa en datastruktur för att lagra tomtarna och deras relationer i
 
 class ElfLand{
-    // current namn är den tomte vars underlydande ni vill ta fram
-    // res är listan som håller alla underlydande
-    fun getUnderlings(currentName: String, res: MutableList<String>): List<String> {
-        //TODO, skriv denna metod, glöm inte att den ska vara rekursiv!
-        throw UnsupportedOperationException()
+
+    val hierarchy = mutableMapOf(
+        "Tomten" to listOf("Glader", "Butter"),
+        "Glader" to listOf("Trötter", "Tröger", "Blyger"),
+        "Butter" to listOf("Rådjuret", "Nyckelpigan", "Haren", "Räven"),
+        "Trötter" to listOf("Skumtomten"),
+        "Skumtomten" to listOf("Dammråttan"),
+        "Räven" to listOf("Gråsuggan", "Myran"),
+        "Myran" to listOf("Bladlusen")
+    )
+
+    fun getUnderlings(currentName : String, hierarchy : Map<String, List<String>>, result : MutableList<String>) : MutableList<String> {
+
+        tailrec fun loop(result: MutableList<String>, index: Int): MutableList<String> {
+            if (index == result.size) return result
+            hierarchy[result[index]]?.let { result.addAll(it) }
+            return loop(result, index + 1)
+        }
+
+        hierarchy[currentName]?.let { result.addAll(it) }
+        return loop(result, 0)
     }
 
-    fun main() {
-
-        //Exempel på anrop till den rekursiva funktionen getUnderlings,
-        // här är tanken att hitta Tröger underlydande
-        //listan fylls på successivt när vi rekurserar
-
-        var list: MutableList<String> = mutableListOf()
-        println(getUnderlings("Tröger", list))
-
-    }
 }
+
+fun main() {
+
+    //Exempel på anrop till den rekursiva funktionen getUnderlings,
+    // här är tanken att hitta Tröger underlydande
+    //listan fylls på successivt när vi rekurserar
+    val ef = ElfLand()
+
+    var list: MutableList<String> = mutableListOf()
+    println(ef.getUnderlings("Tomten", ef.hierarchy, list))
+}
+
+
+//fun getUnderlingsD(currentName : String, hierarchy : Map<String, List<String>>, result : MutableList<String>) : MutableList<String> {
+//
+//    tailrec fun loop(result: MutableList<String>, index: Int): MutableList<String> {
+//        if (index == result.size) return result
+//        // det jag gjorde fel var att jag försökte lägga till från hierarkin med currentname
+////        result.addAll(hierarchy[currentName]!!)
+//        // när jag egentligen borde plockat ut namnen som lagts in i result med hjälp av index, sätta in det i hierarkin,
+//        // därefter adderat de underliggande underlings till det namnet
+//        hierarchy[result[index]]?.let { result.addAll(it) } // varv 1: plockar ut glader och lägger till --- trötter, tröger och blyger
+//                                                            // varv 2: plockar ut butter och lägger till --- "Rådjuret", "Nyckelpigan", "Haren", "Räven"
+//                                                            // varv 3: plockar ut trötter --- lägger till skumtomten
+//                                                            // varv 4: plockar ut tröger, hittar inget
+//                                                            // varv 5: plockar ut bluger, hittar inget
+//                                                            // varv 6: plockar ut rådjuret, hittar inget
+//                                                            // varv 7: plockar ut nyckelpigan, hittar inget
+//                                                            // varv 8: plockar ut haren, hittar inget
+//                                                            // varv 9: plockar ut räven --- lägger till "Gråsuggan", "Myran"
+//                                                            // varv 10: plockar ut skumtomten --- lägger till dammråttan
+//                                                            // varv 11: plockar ut gråsuggan, hittar inget
+//                                                            // varv 12: plockar ut myran --- lägger till bladlusen
+//                                                            // varv 13: plockar ut dammråttan, hittar inget
+//        return loop(result, index + 1)
+//    }
+//    result.addAll(hierarchy[currentName]!!) // lägger till de första underlings till tomten, glader och butter
+//    return loop(result, 0)
+//}
--- test/ElfLandTest.kt
+++ test/ElfLandTest.kt
@@ -27,88 +27,88 @@ class ElfLandTest{
 
     @Test
     fun getUnderlingsBladlusenTest() {
-        val list: List<String> = ArrayList()
-        val underlings: List<String> = tl.getUnderlings("Bladlusen", list)
+        val list: MutableList<String> = ArrayList()
+        val underlings: List<String> = tl.getUnderlings("Bladlusen", tl.hierarchy, list)
         assertEquals(underlings.size, 0)
     }
 
     @Test
     fun getUnderlingsTrögerTest() {
-        val list: List<String> = ArrayList()
-        val underlings: List<String> = tl.getUnderlings("Tröger", list)
+        val list: MutableList<String> = ArrayList()
+        val underlings: List<String> = tl.getUnderlings("Tröger", tl.hierarchy, list)
         assertEquals(underlings.size, 0)
     }
 
     @Test
     fun getUnderlingsBlygerTest() {
-        val list: List<String> = ArrayList()
-        val underlings: List<String> = tl.getUnderlings("Blyger", list)
+        val list: MutableList<String> = ArrayList()
+        val underlings: List<String> = tl.getUnderlings("Blyger", tl.hierarchy, list)
         assertEquals(underlings.size, 0)
     }
 
     @Test
     fun getUnderlingsDammråttanTest() {
-        val list: List<String> = ArrayList()
-        val underlings: List<String> = tl.getUnderlings("Dammråttan", list)
+        val list: MutableList<String> = ArrayList()
+        val underlings: List<String> = tl.getUnderlings("Dammråttan", tl.hierarchy, list)
         assertEquals(underlings.size, 0)
     }
 
     @Test
     fun getUnderlingsGråsugganTest() {
-        val list: List<String> = ArrayList()
-        val underlings: List<String> = tl.getUnderlings("Gråsuggan", list)
+        val list: MutableList<String> = ArrayList()
+        val underlings: List<String> = tl.getUnderlings("Gråsuggan", tl.hierarchy, list)
         assertEquals(underlings.size, 0)
     }
 
     @Test
     fun getUnderlingsHarenTest() {
-        val list: List<String> = ArrayList()
-        val underlings: List<String> = tl.getUnderlings("Haren", list)
+        val list: MutableList<String> = ArrayList()
+        val underlings: List<String> = tl.getUnderlings("Haren", tl.hierarchy, list)
         assertEquals(underlings.size, 0)
     }
 
     @Test
     fun getUnderlingsNyckelpiganTest() {
-        val list: List<String> = ArrayList()
-        val underlings: List<String> = tl.getUnderlings("Nyckelpigan", list)
+        val list: MutableList<String> = ArrayList()
+        val underlings: List<String> = tl.getUnderlings("Nyckelpigan", tl.hierarchy, list)
         assertEquals(underlings.size, 0)
     }
 
     @Test
     fun getUnderlingsRådjuretTest() {
-        val list: List<String> = ArrayList()
-        val underlings: List<String> = tl.getUnderlings("Rådjuret", list)
+        val list: MutableList<String> = ArrayList()
+        val underlings: List<String> = tl.getUnderlings("Rådjuret", tl.hierarchy, list)
         assertEquals(underlings.size, 0)
     }
 
     @Test
     fun getUnderlingsMyranTest() {
-        val list: List<String> = ArrayList()
-        val underlings: MutableList<String> = tl.getUnderlings("Myran", list)
+        val list: MutableList<String> = ArrayList()
+        val underlings: MutableList<String> = tl.getUnderlings("Myran", tl.hierarchy, list)
         assertEquals(underlings.size, uMyran.size)
         assertFalse(underlings.retainAll(uMyran))
     }
 
     @Test
     fun getUnderlingsRävenTest() {
-        val list: List<String> = LinkedList()
-        val underlings: MutableList<String> = tl.getUnderlings("Räven", list)
+        val list: MutableList<String> = LinkedList()
+        val underlings: MutableList<String> = tl.getUnderlings("Räven", tl.hierarchy, list)
         assertEquals(underlings.size, uRäven.size)
         assertFalse(underlings.retainAll(uRäven))
     }
 
     @Test
     fun getUnderlingsSkumtomtenTest() {
-        val list: List<String> = LinkedList()
-        val underlings: MutableList<String> = tl.getUnderlings("Skumtomten", list)
+        val list: MutableList<String> = LinkedList()
+        val underlings: MutableList<String> = tl.getUnderlings("Skumtomten", tl.hierarchy, list)
         assertEquals(underlings.size, uSkumtomten.size)
         assertFalse(underlings.retainAll(uSkumtomten))
     }
 
     @Test
     fun getUnderlingsTrötterTest() {
-        val list: List<String> = LinkedList()
-        val underlings: MutableList<String> = tl.getUnderlings("Trötter", list)
+        val list: MutableList<String> = LinkedList()
+        val underlings: MutableList<String> = tl.getUnderlings("Trötter", tl.hierarchy, list)
         assertEquals(underlings.size, uTrötter.size)
         assertFalse(underlings.retainAll(uTrötter))
     }
@@ -116,24 +116,24 @@ class ElfLandTest{
 
     @Test
     fun getUnderlingsButterTest() {
-        val list: List<String> = LinkedList()
-        val underlings: MutableList<String> = tl.getUnderlings("Butter", list)
+        val list: MutableList<String> = LinkedList()
+        val underlings: MutableList<String> = tl.getUnderlings("Butter", tl.hierarchy, list)
         assertEquals(underlings.size, uButter.size)
         assertFalse(underlings.retainAll(uButter))
     }
 
     @Test
     fun getUnderlingsGladerTest() {
-        val list: List<String> = LinkedList()
-        val underlings: MutableList<String> = tl.getUnderlings("Glader", list)
+        val list: MutableList<String> = LinkedList()
+        val underlings: MutableList<String> = tl.getUnderlings("Glader", tl.hierarchy, list)
         assertEquals(underlings.size, uGlader.size)
         assertFalse(underlings.retainAll(uGlader))
     }
 
     @Test
     fun getUnderlingsTomtenTest() {
-        val list: List<String> = LinkedList()
-        val underlings: MutableList<String> = tl.getUnderlings("Tomten", list)
+        val list: MutableList<String> = LinkedList()
+        val underlings: MutableList<String> = tl.getUnderlings("Tomten", tl.hierarchy, list)
         assertEquals(underlings.size, uTomten.size)
         assertFalse(underlings.retainAll(uTomten))
     }
